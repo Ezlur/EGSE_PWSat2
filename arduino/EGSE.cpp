0e0188a21b965c80d8506db650f5e58f2dbaef2f
Ezlur <smialekadam@gmail.com>
2016-08-30 14:49:03 +0200

    add .cpp

--- arduino/EGSE.cpp
+++ arduino/EGSE.cpp
@@ -1,19 +1,21 @@
 #include <util/delay.h>
 #include "DigitalIO.h"
 #include "boards.h"
-
+#include "TWI.h"
+#include "Serial.h"
 
 int main() {
+	
+	// LED blink, just to have a hardware proof that program was uploaded
     constexpr hal::DigitalIO pin(hal::bsp::pins::LED);
-
     pin.init(hal::DigitalIO::OUTPUT);
-
     pin.reset();
+	_delay_ms(2500);
+    pin.set();
+	_delay_ms(500);
 
-    while (true) {
-         pin.reset();
-		_delay_ms(2500);
-        pin.set();
-		_delay_ms(500);
-    }
+	// serial communication
+	hal::Serial0::init(9600)
+	
+
 }
