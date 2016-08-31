c8c5123d5db87ae7d0a87f796d981bb50a9864ad
Ezlur <smialekadam@gmail.com>
2016-08-31 15:29:16 +0200

    reorder scripts,

--- host/add_pinout.py
+++ host/add_pinout.py
@@ -38,12 +38,26 @@ def write_pins():
 
     for key in sorted(pinout.keys()):
         while True:
-            pin = (input(key + ': '))
+            pin = input(key + ': ')
             if is_pin_ok(pin): break
         pinout[key] = pin
 
     with open("pinout.txt", 'w') as file:
         file.write(str(pinout))
+
+
+def read_pins():
+    try:
+        file = open('pinout.txt', 'r')
+    except IOError as e:
+            if e.errno == 2:
+                print('There is no \"pinout.txt\" file.')
+                write_pins()
+                file = open('pinout.txt', 'r')
+            else:
+                print("I/O error({0}): {1}".format(e.errno, e.strerror))
+    dictionary = eval(file.read())
+    return dictionary
 
 
 if __name__ == "__main__":
--- host/script.py
+++ host/script.py
@@ -1,20 +1,17 @@
-from add_pinout import write_pins
+import add_pinout
+from lib import *
 
 
-def read_pins():
-    try:
-        file = open('pinout.txt', 'r')
-    except IOError as e:
-            if e.errno == 2:
-                print('There is no \"pinout.txt\" file.')
-                write_pins()
-                file = open('pinout.txt', 'r')
-            else:
-                print("I/O error({0}): {1}".format(e.errno, e.strerror))
-    dictionary = eval(file.read())
-    return dictionary
+def
 
 
 if __name__ == "__main__":
-    pinout = read_pins()
+    pinout = {}
+    pinout.update(add_pinout.read_pins())
     print(pinout)
+    for key in pinout:
+        print pinout[key]
+
+    pin = raw_input("Input here: ")
+    #pin = '\'' + pin + '\''
+    print pinout[pin]
